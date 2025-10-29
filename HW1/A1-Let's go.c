128df50efe5699a95c74bf75ac813d2f61c6db97
lerol_0 <runstud@ya.ru>
2025-10-29 15:15:15 +0500

    Create A1-Let's go.c
    A1-Let's go

---
+++ HW1/A1-Let's go.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+//	 Let's
+//           go
+//             to walk
+int main(int argc, char **argv)
+{
+	printf("	Let's\n");
+	printf("		go\n");
+	printf("			to walk\n");
+	
+	return 0;
+}
+

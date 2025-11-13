45a5452c078e0672d7ec03eb5ceb63abb8bf7d5c
lerol_0 <runstud@ya.ru>
2025-11-13 16:18:37 +0500

    Create A6.c
    A6-Разность двух чисел

---
+++ HW3/A6.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int n=0;
+    int sum;
+    scanf("%d", &n);
+    sum = n%10; 
+    sum *= (n/10)%10; 
+    sum *= (n/100)%10; 
+    printf("%d\n",sum);
+    return 0;
+}

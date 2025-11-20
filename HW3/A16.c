e403a51653e8bd371664ab8cc77c823065cb9664
lerol_0 <runstud@ya.ru>
2025-11-20 17:34:59 +0500

    Create A16.c
    A16-В порядке возрастания

---
+++ HW3/A16.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+	int a=0;
+	int b=0;
+	int c=0;
+    scanf("%d%d%d", &a,&b,&c);
+    if((a<b) &&(b<c)){
+		printf("YES");
+		} else {
+    printf("NO");
+}
+    return 0;
+}
